39affe2ad7c3137f0ac5d0f88c8c5722f6855f85
miyuru <miyuruabeysiriwardana13@gmail.com>
2024-04-16 23:13:28 +0530

    changes

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -15,28 +15,36 @@ const Sidebar = () => {
       navigate("/");
     }
   };
-  console.log("Menu",menu)
+
   return (
     <div className="sticky top-0 h-[100vh]">
       <div className="flex flex-col justify-between h-full px-10">
         <div>
           <div className="pt-10">
             <img
-              className="w-40"
+              className="w-28"
               src="https://i.imgur.com/zqpwkLQ.png"
               alt=""
             />
           </div>
-          {/* <div className="mt-10">
-            {menu.map((item) => (
-              <div 
-            //   onClick={() => handleTabClick(item.title)} 
-              className="flex items-center mb-5 cursor-pointer textlg">
-                {activeTab===item.title? item.activeIcon:item.icon}
-                <p className={`${activeTab===item.title?"font-bold":"font-semibold"}`}>{item.title}</p>
+          <div className="mt-10 flex flex-col">
+            {menu.map((item, index) => (
+              <div
+                key={index}
+                onClick={() => handleTabClick(item.title)}
+                className="flex flex-row items-center mb-5 cursor-pointer text-lg"
+              >
+                {activeTab === item.title ? item.activeIcon : item.icon}
+                <p
+                  className={`${
+                    activeTab === item.title ? "font-semibold" : ""
+                  }`}
+                >
+                  {item.title}
+                </p>
               </div>
             ))}
-          </div> */}
+          </div>
         </div>
         <div className="flex items-center cursor-pointer pb-10">
           <IoReorderThreeOutline />
